085f252e0e5b4039828cd7580960ea86c4cfd319
Snehel Patel <snehelpatel92@gmail.com>
2017-01-26 09:46:55 -0500

    changed content for projects about

--- src/js/projarray.js
+++ src/js/projarray.js
@@ -5,7 +5,7 @@ const allProjects = [
 	{
 		link: 'ironstack',
 		title: 'IronStack',
-		blurb: "IronStack is an education app based around the experience of the typical Iron Yard student. Student users are able to register for their particular cohort and interact with other users as they share tips, online resources, words of encouragement based on curriculum and assignments for a given week.",
+		blurb: "IronStack is an education app based around the experience of the typical Iron Yard student. Student users are able to register for their particular cohort and interact with other users as they share tips, online resources, and words of encouragement based on curriculum and assignments for a given week.",
 		deployed: 'http://tiy-lelawalker-ironstackfe.surge.sh/#/start',
     github: 'https://github.com/aliensfinalproject/',
 		thumb: './images/ironstack.png',
@@ -17,7 +17,7 @@ const allProjects = [
 	{
 		link: 'instashare',
 		title: 'InstaShare',
-		blurb: "InstaShare is an Instagram clone and originally a hackathon team project. Users can share images on the site, 'heart' what they like, and comment on exisiting images.",
+		blurb: "InstaShare is an Instagram clone and a hackathon team project. Users can share images on the site, 'heart' what they like, and comment on exisiting images.",
 		deployed: 'https://tiy-barryhoward-hackathon.surge.sh/',
 		github: 'https://github.com/snehelpatel/hackathon',
 		thumb: './images/instashare.png',
@@ -28,9 +28,9 @@ const allProjects = [
 	{
 		link: 'kji',
 		title: 'KJI Public House',
-		blurb: "KJI Public House is a mockup of a restaurant website created by a team of three. We implimented previously learned vanilla Javascript and jQuery Ajax calls and newly-learned Git workflow. ",
-		deployed: 'https://tiy-barryhoward-hackathon.surge.sh/',
-		github: 'http://tiy-snehelpatel-team4restaurantpage.surge.sh/',
+		blurb: "KJI Public House is a mockup of a restaurant website created by a team of three. We implemented previously learned vanilla Javascript and jQuery Ajax calls and newly-learned Git workflow skills. ",
+		deployed: 'http://tiy-snehelpatel-team4restaurantpage.surge.sh/',
+		github: 'https://github.com/team4restaurantpage/team4restaurantpage',
 		thumb: './images/kjipublichouse.png',
 		images: [],
 		tools: "jQuery, Lodash"
